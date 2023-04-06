d874e8ebdcf115afb821c3a61e1c34c9bda49ce3
Magda Slifierz <m.slifierz@gmail.com>
2023-04-05 22:03:29 -0700

    ex

--- shop/recommender.py
+++ shop/recommender.py
@@ -46,3 +46,7 @@ class Recommender:
             id__in=suggested_products_ids ))
         suggested_products.sort(key=lambda x: suggested_products_ids.index(x.id))
         return suggested_products
+    
+    def clear_purchases(self):
+        for id in Product.objects.values_list('id', flat=True):
+            r.delete(self.get_product_key(id))
